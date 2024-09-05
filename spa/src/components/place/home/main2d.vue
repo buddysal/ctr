2884ad9d3f49df6bd1cf764cc2ce0f7cc72b76a3
buddysal <63454652+buddysal@users.noreply.github.com>
2024-09-05 07:23:51 -0500

    Reapply "Update main2d.vue"
    This reverts commit 9dad3421a13ca7a18c7e1550d28e562e025b206c.

--- spa/src/components/place/home/main2d.vue
+++ spa/src/components/place/home/main2d.vue
@@ -47,19 +47,7 @@
             <td class="font-bold text-left">
               Last Access
             </td>
-              <!--              
-              Checks if date is Monday, August 12 2024.
-              If true, pull date last daily xp was paid out instead.
-              Fixes people having the same date that have not logged
-              in since the "Places" update was added.
-              Will still blanket a date for anyone
-              that have not logged on since the xp payout was added.
-              -->
-            <td class="text-left" v-if="date">
-              <!-- format Saturday, October 9 1999. -->
-              {{ memberInfo.last_daily_login_credit | dateFormatFilter }}
-            </td>
-            <td class="text-left" v-else>
+            <td class="text-left">
               <!-- format Saturday, October 9 1999. -->
               {{ memberInfo.lastAccess | dateFormatFilter }}
             </td>
@@ -135,7 +123,6 @@ export default Vue.extend({
       memberInfo: {},
       canAdmin: false,
       loaded: false,
-      date: false,
 
     };
   },
@@ -146,10 +133,6 @@ export default Vue.extend({
       try {
         const response = await this.$http.get("/member/info/"+this.$store.data.place.member_id);
         this.memberInfo = response.data.memberInfo;
-        const splitDate = this.memberInfo.lastAccess.split("T");
-        if(splitDate[0] === '2024-08-12'){
-          this.date = true;
-        } 
       } catch (error) {
         console.log(error);
       }
